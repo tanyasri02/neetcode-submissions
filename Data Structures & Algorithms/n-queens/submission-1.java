bcbba11de4081a8dd3d1b7242804bdf0d7485841
Tanya Srivastava <59734200+tanyasri02@users.noreply.github.com>
2026-09-21 21:34:24 +0530

    Add: n-queens - submission-1

---
+++ Data Structures & Algorithms/n-queens/submission-1.java
@@ -0,0 +1,72 @@
+class Solution {
+    public boolean isSafe(char[][] board, int row, int col){
+        int n = board.length;
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q')
+                return false;
+        }
+        
+        // up
+        for(int j=0;j<col;j++){
+            if(board[row][j] == 'Q')
+                return false;
+        }
+
+        // upper diagonal
+
+        for(int i=row,j=col; i>=0 && j>=0; i--,j--){
+            if(board[i][j] == 'Q')
+                return false;
+        }
+
+        // lower diagonal
+        for(int i=row,j=col; i < n && j>=0; i++,j--){
+            if(board[i][j] == 'Q')
+                return false;
+        }
+
+        return true;
+    }
+
+    public void helper(char[][] board, int col, int n, 
+    List<List<String>> ans){
+        if(col == n){
+            ans.add(constructBoard(board));
+            return;
+        }
+
+        for(int row=0;row<n;row++){
+            if(isSafe(board, row, col)){
+                board[row][col] = 'Q';
+                helper(board, col+1, n, ans);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    List<String> constructBoard(char[][] board){
+        List<String> curr = new ArrayList<>();
+
+        for(int i=0;i<board.length;i++){
+            curr.add(new String(board[i]));
+        }
+
+        return curr;
+    }
+
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j] = '.';
+            }
+        }
+
+        List<List<String>> ans = new ArrayList<>();
+
+        helper(board, 0, n, ans);
+
+        return ans;
+    }
+}
